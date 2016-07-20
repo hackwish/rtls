1c075cc746930384c010162d8ba9c535bf0570de
Marcelo C <marcelo.cardenasc@gmail.com>
2016-07-20 00:55:30 -0400

    mejoras en calculo tridimensional

--- src/Modelo/Negocio.java
+++ src/Modelo/Negocio.java
@@ -22,16 +22,24 @@ public class Negocio {
         if( !valorx.isEmpty() && !valory.isEmpty() && !rssi1.isEmpty() && !rssi2.isEmpty() && !rssi3.isEmpty()){
                 double x =  Double.parseDouble(valorx);
                 double y =  Double.parseDouble(valory);
-                double i = 0;
+                int i = 0;
+                int j = 0;
+                int d = 0;
                 double R1 =  Double.parseDouble(rssi1);
                 double R2 =  Double.parseDouble(rssi2);
                 double R3 =  Double.parseDouble(rssi3);
+                double divx=0;
+                double divy=0;
+                double vx=0;
                 //float resux = 10;
                 //float resuy = 10;
-                i = (2/x);
-                
-                double resux = (((Math.pow(R1,2))-(Math.pow(R2,2))+(Math.pow(x,2)))/(2*x));
-                double resuy = (((Math.pow(R1,2))-(Math.pow(R3,2))-(Math.pow(x,2)+(Math.pow((x-i),2))+Math.pow(y,2)))/(2*y));
+                i =2;
+                d=9;
+                j=14;
+                divx=(2*d);
+                divy=(-2*j);
+                double resux = (((R1-R2))+(Math.pow(d,2)))/divx;
+                double resuy = (((R3-R1))+(Math.pow(resux,2))-(Math.pow((resux-i),2))-(Math.pow(j,2)))/divy;
                 //float resux = (((Math.pow(rssi1,2)) - (Math.pow(rssi2,2)) + (Math.pow(valorx,2)))/float((2*valorx)));
                 //float resuy = ((Math.pow(R1,2))-(Math.pow(R3,2))-(Math.pow(x,2)+(Math.pow((x-i),2))+Math.pow(y,2))/float(2*y));             
                 
